ce18b28f25c8bebbc6853c22df10e0fcb4572c45
GJ Tiquia <gershom.tiquia@gmail.com>
2025-02-01 19:03:06 -0500

    chore: setup absolute paths and partially setup shadcn

--- web-app/src/routes/index.tsx
+++ web-app/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useState, useEffect } from 'react';
-import { client } from '../backend';
+import { client } from '@/backend';
 
 export const Route = createFileRoute('/')({
     component: Index,
