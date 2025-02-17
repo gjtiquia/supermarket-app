2b310b701fb622b62cfc027156e97c0088278e68
GJ Tiquia <gershom.tiquia@gmail.com>
2025-02-16 21:01:46 -0500

    feat: add sign-up form component with validation and authentication

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -1,15 +1,12 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
-import { user, session } from "@/db/schema";
+import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
-        schema: {
-            user: user,
-            session: session,
-        }
+        schema
     }),
     emailAndPassword: {
         enabled: true
--- web-app/src/routes/index.tsx
+++ web-app/src/routes/index.tsx
@@ -1,28 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { authClient } from '@/lib/auth'
+import { SignUpForm } from '@/components/signUpForm'
+
 export const Route = createFileRoute('/')({
     component: Index,
 })
 
 function Index() {
-
-    // TODO: Docs: https://www.better-auth.com/docs/authentication/email-password
-
-    async function handleLoginAsync() {
-        const response = await authClient.signUp.email({
-            email: "test@test.com",
-            password: "test",
-            name: "test",
-        })
-
-        console.log(response)
-    }
-
     return (
-        <div className="container mx-auto py-6 space-y-6">
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button onClick={handleLoginAsync}>Login</button>
+        <div className="container mx-auto py-6 max-w-md">
+            <SignUpForm />
         </div>
     )
 }
