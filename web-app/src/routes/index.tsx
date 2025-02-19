644b83c384681f2f6643a98c888d2ba6c0c8fd89
GJ Tiquia <gershom.tiquia@gmail.com>
2025-02-18 22:13:14 -0500

    delete unused signUpForm

--- web-app/src/routes/index.tsx
+++ web-app/src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { SignUpForm } from '@/components/signUpForm'
 import { SignIn } from '@/components/auth/sign-in'
 import { SignUp } from '@/components/auth/sign-up'
 
