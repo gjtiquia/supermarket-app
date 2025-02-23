2979b6bf1c9b8056bfcc38d8fd08830d67968e6b
GJ Tiquia <gershom.tiquia@gmail.com>
2025-02-22 23:30:38 -0500

    fix: vertical scroll overflow

--- web-app/src/routes/__root.tsx
+++ web-app/src/routes/__root.tsx
@@ -5,9 +5,11 @@ import { Nav } from '@/components/nav'
 
 export const Route = createRootRoute({
     component: () => (
-        <div className="h-dvh pb-[4.5rem] md:pb-0 md:pl-[3.5rem] xl:pl-52">
-            <Nav />
-            <Outlet />
+        <div className="min-h-dvh flex flex-col">
+            <div className="flex-1 md:pl-[3.5rem] xl:pl-52 pb-[4.5rem] md:pb-0">
+                <Nav />
+                <Outlet />
+            </div>
             <Toaster />
             {/* {import.meta.env.DEV && <TanStackRouterDevtools />} */}
         </div>
